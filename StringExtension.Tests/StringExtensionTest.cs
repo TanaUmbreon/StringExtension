6b87dafdd793f0f23ffd3b71377789c6c17e056e
Tana <finaltruth.shintai@gmail.com>
2017-12-02 11:39:32 +0900

    .NET StandardのクラスライブラリプロジェクトだとNUnitが動かないので.NET Frameworkのクラスライブラリに変更。 MidBメソッドを実装。

---
+++ StringExtension.Tests/StringExtensionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringExtension.Tests
+{
+    [TestFixture]
+    public class StringExtensionTest
+    {
+        [Test]
+        public void MidB()
+        {
+            Assert.AreEqual(0, "".LenB()); // 空文字
+            Assert.AreEqual(4, "\t\r\n ".LenB()); // エスケープ文字
+            Assert.AreEqual(24, "科学の　力って　すげー！".LenB()); // 全角のみ
+            Assert.AreEqual(19, "Science is amazing!".LenB()); // 半角のみ
+            Assert.AreEqual(21, "Science　is　amazing!".LenB()); // 全角半角混在
+
+            // null値
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                string nullText = null;
+                nullText.LenB();
+            });
+        }
+    }
+}
---
+++ StringExtension/StringExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringExtension
+{
+    /// <summary>
+    /// <see cref="String"/> クラスの拡張メソッドを提供します。
+    /// </summary>
+    public static class StringExtension
+    {
+        #region フィールド
+
+        /// <summary>Shift-JIS の文字エンコーディング</summary>
+        private static readonly Encoding ShiftJis = Encoding.GetEncoding("sjis");
+
+        #endregion
+
+        #region LenB
+
+        /// <summary>
+        /// 文字列を Shift-JIS として扱った場合のバイト数を取得します。
+        /// </summary>
+        /// <param name="value">文字列。</param>
+        /// <returns>文字列のバイト数。</returns>
+        public static int LenB(this string value)
+        {
+            return ShiftJis.GetByteCount(value);
+        }
+
+        #endregion
+    }
+}
