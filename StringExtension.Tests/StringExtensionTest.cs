5ba5ac6384a60ec0aaf1ab9f6c26d3bdd09d3090
Tana <finaltruth.shintai@gmail.com>
2017-12-03 18:14:07 +0900

    MidBメソッドを実装。

--- StringExtension.Tests/StringExtensionTest.cs
+++ StringExtension.Tests/StringExtensionTest.cs
@@ -9,18 +9,78 @@ namespace StringExtension.Tests
         [Test]
         public void LenB()
         {
-            Assert.AreEqual(0, "".LenB()); // 空文字
-            Assert.AreEqual(4, "\t\r\n ".LenB()); // エスケープ文字
-            Assert.AreEqual(24, "科学の　力って　すげー！".LenB()); // 全角のみ
-            Assert.AreEqual(19, "Science is amazing!".LenB()); // 半角のみ
-            Assert.AreEqual(21, "Science　is　amazing!".LenB()); // 全角半角混在
-
-            // null値
+            // 例外が発生するパターン
             Assert.Throws<ArgumentNullException>(() =>
             {
                 string nullText = null;
                 nullText.LenB();
             });
+
+            Assert.AreEqual(0, "".LenB()); // 空文字
+            Assert.AreEqual(4, "\t\r\n ".LenB()); // エスケープ文字
+            Assert.AreEqual(24, "科学の　力って　すげー！".LenB()); // 全角のみ
+            Assert.AreEqual(19, "Science is amazing!".LenB()); // 半角のみ
+            Assert.AreEqual(21, "Science　is　amazing!".LenB()); // 全角半角混在
+        }
+
+        /// <summary>
+        /// MidB(string, int, int)
+        /// </summary>
+        [Test]
+        public void MidB1()
+        {
+            // 例外が発生するパターン
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                string nullText = null;
+                nullText.MidB(0, 0);
+            });
+            Assert.Throws<ArgumentOutOfRangeException>(() => { "".MidB(-1, 0); });
+            Assert.Throws<ArgumentOutOfRangeException>(() => { "".MidB(0, -1); });
+
+            // 長さがゼロ
+            Assert.AreEqual("", "".MidB(0, 0));
+            Assert.AreEqual("", "Pikachu".MidB(3, 0));
+            Assert.AreEqual("", "ピカチュウ".MidB(4, 0));
+
+            // 開始位置がオーバー
+            Assert.AreEqual("", "".MidB(1, 2));
+            Assert.AreEqual("", "Pikachu".MidB(7, 1));
+            Assert.AreEqual("", "ピカチュウ".MidB(10, 2));
+
+            // 長さがオーバー
+            Assert.AreEqual("", "".MidB(0, 2));
+            Assert.AreEqual("hu", "Pikachu".MidB(5, 3));
+            Assert.AreEqual("ウ", "ピカチュウ".MidB(8, 3));
+
+            Assert.AreEqual("Pika", "Pikachu".MidB(0, 4));
+            Assert.AreEqual("チュウ", "ピカチュウ".MidB(4, 6));
+            Assert.AreEqual("ピ ", "ピカチュウ".MidB(0, 3)); // 全角文字の途中(末尾)
+            Assert.AreEqual("sカ", "ピカチュウ".MidB(1, 3)); // 全角文字の途中(先頭)
+        }
+
+        /// <summary>
+        /// MidB(string, int)
+        /// </summary>
+        [Test]
+        public void MidB2()
+        {
+            // 例外が発生するパターン
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                string nullText = null;
+                nullText.MidB(0);
+            });
+            Assert.Throws<ArgumentOutOfRangeException>(() => { "".MidB(-1); });
+
+            // 開始位置がオーバー
+            Assert.AreEqual("", "".MidB(1));
+            Assert.AreEqual("", "Pikachu".MidB(7));
+            Assert.AreEqual("", "ピカチュウ".MidB(10));
+
+            Assert.AreEqual("Pikachu", "Pikachu".MidB(0));
+            Assert.AreEqual("チュウ", "ピカチュウ".MidB(4));
+            Assert.AreEqual("sカチュウ", "ピカチュウ".MidB(1)); // 全角文字の途中(先頭)
         }
     }
 }
--- StringExtension/StringExtension.cs
+++ StringExtension/StringExtension.cs
@@ -29,5 +29,67 @@ namespace StringExtension
         }
 
         #endregion
+
+        #region MidB
+
+        /// <summary>
+        /// 文字列を Shift-JIS として扱い、バイト単位で指定した位置と長さに該当する部分文字列を取得します。 
+        /// </summary>
+        /// <param name="value">文字列。</param>
+        /// <param name="startIndex">0 から始まるバイト単位の開始位置。</param>
+        /// <param name="length">バイト単位の長さ。</param>
+        /// <returns>
+        /// <paramref name="startIndex"/> から長さ <paramref name="length"/> を抽出することによって得られる部分文字列。
+        /// <paramref name="startIndex"/> が文字列の長さを超えている、または、<paramref name="length"/> がゼロの場合は <see cref="String.Empty"/>。
+        /// <paramref name="startIndex"/> と <paramref name="length"/> で表される部分が文字列を超えている場合は、超えている部分を無視した部分文字列。
+        /// 部分文字列の末尾の文字が途中で分断された全角文字になる場合は、
+        /// その分断された文字を切り詰め、長さ <paramref name="length"/> になるまで半角スペースで埋めます。
+        /// </returns>
+        public static string MidB(this string value, int startIndex, int length)
+        {
+            if (value == null) { throw new ArgumentNullException(nameof(value)); }
+            if (startIndex < 0) { throw new ArgumentOutOfRangeException(nameof(startIndex), "開始位置を 0 未満にすることはできません。"); }
+            if (length < 0) { throw new ArgumentOutOfRangeException(nameof(length), "長さを 0 未満にすることはできません。"); }
+
+            // 空文字が確定している場合は無駄な処理をさせないようすぐ返す
+            if (length == 0) { return ""; }
+            var bytes = ShiftJis.GetBytes(value);
+            if (startIndex >= bytes.Length) { return ""; }
+
+            // 長さがオーバーしているとGetStringで例外になるので長さ調整してから部分文字列を取得
+            var adjustedLength = (bytes.Length < startIndex + length) ? bytes.Length - startIndex : length;
+            var result = ShiftJis.GetString(bytes, startIndex, adjustedLength);
+
+            // 末尾にある全角文字の途中を抽出すると長さがずれることがある。
+            // その場合は末尾を切り詰めて半角スペースで埋める(Shift-JIS前提で決め打ち)
+            if (ShiftJis.GetByteCount(result) == adjustedLength) { return result; }
+            return ShiftJis.GetString(bytes, startIndex, adjustedLength - 1) + ' ';
+        }
+
+        /// <summary>
+        /// 文字列を Shift-JIS として扱い、バイト単位で指定した位置から始まる部分文字列を返します。
+        /// </summary>
+        /// <param name="value">文字列。</param>
+        /// <param name="startIndex">0 から始まるバイト単位の開始位置。</param>
+        /// <returns>
+        /// <paramref name="startIndex"/> から始まる部分文字列。
+        /// <paramref name="startIndex"/> が文字列の長さを超えている場合は <see cref="String.Empty"/>。
+        /// </returns>
+        public static string MidB(this string value, int startIndex)
+        {
+            // MidB(string, startIndex, length)のオーバーロードは呼び出さず独立したメソッドにしている。
+            // 末尾の全角文字の途中を抽出するリスクがなく実装がシンプルになる為
+
+            if (value == null) { throw new ArgumentNullException(nameof(value)); }
+            if (startIndex < 0) { throw new ArgumentOutOfRangeException(nameof(startIndex), "開始位置を 0 未満にすることはできません。"); }
+
+            // 開始位置が文字列の長さを超えた場合は空文字確定なのですぐに返す
+            var bytes = ShiftJis.GetBytes(value);
+            if (bytes.Length <= startIndex) { return ""; }
+
+            return ShiftJis.GetString(bytes, startIndex, bytes.Length - startIndex);
+        }
+
+        #endregion
     }
 }
