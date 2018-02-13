19425c42197d2037dd458554035fbea47c4e38dc
Tana <finaltruth.shintai@gmail.com>
2018-02-13 23:29:08 +0900

    テストプロジェクトのルート名前空間、アセンブリ名を統一。

--- projects/StringExtension.NetStd13.Tests/Properties/AssemblyInfo.cs
+++ projects/StringExtension.NetStd13.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("StringExtension.Tests")]
+[assembly: AssemblyTitle("StringExtension.NetStd13.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StringExtension.Tests")]
+[assembly: AssemblyProduct("StringExtension.NetStd13.Tests")]
 [assembly: AssemblyCopyright("Copyright © 2017 Tana")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
